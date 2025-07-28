fe2a359da6b256fa1a54cfda37abf6eff8af521e
Nikita Filippenkov <666disciplina666@gmail.com>
2025-07-28 21:40:43 +0300

    test static

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 python manage.py migrate
 python manage.py collectstatic --noinput
-cp -r /app/collected_static/. /backend_static/static/
+cp -r /app/collected_static/. /backend_static/
 exec "$@"
